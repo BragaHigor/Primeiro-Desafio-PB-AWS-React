3d962f725e26fcc18efb394d45bf06eece94cce9
Higor Braga <higorbraga.s@gmail.com>
2022-12-26 12:16:08 -0300

    Reponsividade Concluido

--- js/textarea.js
+++ js/textarea.js
@@ -1,5 +1,17 @@
-var textarea = document.getElementById("textarea");
-var caracteresRestantes = document.getElementById("caracteresRestantes");
-textarea.oninput = function(){
-    caracteresRestantes.innerHTML = (`${130 - this.value.length} allowed characters.`);
+const contador = () => {
+
+    var textarea = document.getElementById("textarea");
+    var caracteresRestantes = document.getElementById("caracteresRestantes");
+    textarea.oninput = function () {
+        caracteresRestantes.innerHTML = (`${130 - this.value.length} allowed characters.`);
+    }
+
+    var caractere = 130 - (document.getElementById("textarea")).value.length
+
+    if(caractere <= 130) {
+        const nextBtn = document.getElementById("next")
+
+        nextBtn.setAttribute('disabled', '')
+    }
+
 }
